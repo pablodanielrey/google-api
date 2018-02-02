c251b44bde75a46f6ef01484fb29b65c688b45dd
Pablo Daniel Rey <pablodanielrey@gmail.com>
2018-02-02 13:03:40 -0300

    le hago commit a ema para que pueda arrancar a verlo

--- src/google/api/rest/main.py
+++ src/google/api/rest/main.py
@@ -51,14 +51,11 @@ def actualizarUsuario(uid):
     ''' toma de la base de usuarios los datos y lo sincroniza internamente con la base del sistema de google '''
     return GoogleModel.actualizarUsuarios(uid)
 
-
-
 # sincroniza las claves pendientes con google
 @app.route('/google/api/v1.0/sincronizar_claves/', methods=['GET'])
 @jsonapi
 def sincronizarClaves():
     return GoogleModel.sincronizarClaves()
-
 
 # actualiza los usuarios pendientes con Google, si no existen los crea
 @app.route('/google/api/v1.0/sincronizar/', methods=['GET'])
